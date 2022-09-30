cec29423190bca51b10bef8f5082c2f0d976e6a8
JuanConti <juanpcsilva@gmail.com>
2022-09-30 18:14:10 -0300

    splash and background screen

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,12 @@
+import { View, ActivityIndicator } from 'react-native';
+
+import { styles } from './styles';
+
+export function Loading() {
+  return (
+    <View style={styles.container}>
+
+    <ActivityIndicator style={styles.container} />
+    </View>
+  );
+}
